343503276d17c201c0cfd19be9735c3dfd5597a6
CodeDotJS <rushforlinux@gmail.com>
2016-04-18 00:36:17 +0530

    finisehd

--- cli.js
+++ cli.js
@@ -8,10 +8,42 @@ const colors = require('colors');
 
 const argv = require('yargs')
 
-    .usage(colors.cyan.bold('  \nUsage: $0 -u [user.name]'))
+    .usage(colors.cyan.bold('\nUsage: $0 -u [user.name]'))
 
     .demand(['u'])
 
     .describe('u', 'Twitter username')
 
     .argv;
+
+twifo(argv.u).then(user => {
+	const inf = [];
+
+	const informationRow = (prefix, key) => {
+		if (user[key]) {
+			inf.push(`${prefix}: ${user[key]}`);
+		}
+	};
+
+	console.log('\n');
+
+	informationRow(' ❱ Name      ', 'name');
+
+	informationRow(' ❱ Handle    ', 'handle');
+
+	informationRow(' ❱ Bio       ', 'bio');
+
+	informationRow(' ❱ Joined    ', 'joined');
+
+	informationRow(' ❱ Tweets    ', 'tweets');
+
+	informationRow(' ❱ Following ', 'following');
+
+	informationRow(' ❱ Followers ', 'followers');
+
+	informationRow(' ❱ Likes     ', 'likes');
+
+	console.log(inf.join('\n'));
+
+	console.log('\n');
+});
